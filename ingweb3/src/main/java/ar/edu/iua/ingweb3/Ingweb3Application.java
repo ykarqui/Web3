429c87767a837d5d15be2b615039a2c68ebc0086
ykarqui <ykarqui@gmail.com>
2018-08-15 14:23:36 -0300

    segundo

---
+++ ingweb3/src/main/java/ar/edu/iua/ingweb3/Ingweb3Application.java
@@ -0,0 +1,12 @@
+package ar.edu.iua.ingweb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ingweb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ingweb3Application.class, args);
+	}
+}
---
+++ ingweb3/src/test/java/ar/edu/iua/ingweb3/Ingweb3ApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.edu.iua.ingweb3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ingweb3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
